f0eb116a764a065599c648f6a9af1f4bc154702c
Ahri <ahripyx@gmail.com>
2025-06-06 23:03:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using Lytheria.config;
+
+namespace Lytheria
+{
+    internal class Program
+    {
+        // Discord client instance to be used throughout the application.
+        private static DiscordClient client { get; set; }
+        // CommandsNextExtension instance to handle commands.
+        private static CommandsNextExtension commands { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            client = new DiscordClient(discordConfig);
+
+            client.Ready += Client_Ready;
+
+            await client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ config/JSONReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.Net.Serialization;
+using Newtonsoft.Json;
+
+namespace Lytheria.config
+{
+    internal class JSONReader
+    {
+        //reads json file
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+        public async Task ReadJSON()
+        {
+            // Reads the JSON config
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                JSONStructure data = JsonConvert.DeserializeObject<JSONStructure>(json);
+
+                this.token = data.token;
+                this.prefix = data.prefix;
+            }
+        }
+    }
+
+    internal sealed class JSONStructure
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
